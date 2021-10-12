df62ef94bbb04ec07f09e4e35c49b017e24d272b
Jonatan Luis Moreno <jonatanluismoreno@gmail.com>
2021-10-12 19:31:24 -0300

    add Navbar component

---
+++ src/components/molecules/Navbar/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './styles.css'
+
+interface Props {
+  
+}
+
+
+const Navbar: React.FC<Props> = () => {
+  return (
+    <header className='navbar'>
+      
+    </header>
+  )
+}
+
+export default Navbar
