ad04fa3dc9e5dd67684975f18d4bee91173f250c
Jonatan Luis Moreno <jonatanluismoreno@gmail.com>
2022-03-23 00:26:02 -0300

    Updated Navbar component

---
+++ src/assets/SearchIcon.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const SearchIcon = ({
+  height="32px",
+  width="32px",
+}: React.SVGProps<SVGSVGElement>) => (
+  <svg 
+  fill="#ffffff" 
+  xmlns="http://www.w3.org/2000/svg"  
+  viewBox="0 0 40 40" 
+  width={width} 
+  height={height}
+  >
+    <path d="M24.926 22.158c1.895-2.728 1.627-6.504-.805-8.936-2.734-2.733-7.166-2.733-9.9 0-2.733 2.734-2.733 7.166 0 9.9 2.432 2.432 6.21 2.7 8.938.804l5.735 5.736 1.768-1.768-5.736-5.736zm-2.573-7.168c1.757 1.757 1.757 4.606 0 6.364-1.757 1.757-4.607 1.757-6.364 0-1.757-1.758-1.757-4.607 0-6.364 1.757-1.758 4.607-1.758 6.364 0z" fillRule="evenodd" fillOpacity="1" fill="white" stroke="none"></path>
+  </svg>
+);
--- src/components/molecules/Navbar/index.tsx
+++ src/components/molecules/Navbar/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './styles.css'
 
+import searchheader from '../../../assets/searchheader.svg'
+import community from '../../../assets/community.svg'
+import arrowheader from '../../../assets/arrowheader.svg'
+import user from '../../../assets/user.png'
 interface Props {
   
 }
@@ -9,7 +13,24 @@ interface Props {
 const Navbar: React.FC<Props> = () => {
   return (
     <header className='navbar'>
-      
+      <section className='left-section'>
+        <div className='community-account'>
+          <img src={community} alt="" className='community-svg'/>
+          <img src={arrowheader} alt="" className='arrow-svg' />
+        </div>
+        <span className='text'>Community</span>
+        <div className='search-community'>
+          <img src={searchheader} alt="" className='search-svg'/>
+          <input type="text" placeholder='Search Community' className='input'/>
+        </div>
+      </section>
+      <section className="right-section">
+        <button className="button-header">Publish</button>
+        <div className="profile-container">
+          <img src={user} alt="" className='user-image'/>
+          <img src={arrowheader} alt="" className='arrow-svg'/>
+        </div>
+      </section>
     </header>
   )
 }
