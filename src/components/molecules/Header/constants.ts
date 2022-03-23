f27e0671c81141b8b8de800fb1abc0bf317f8492
Jonatan Luis Moreno <jonatanluismoreno@gmail.com>
2022-03-23 00:32:13 -0300

    Header Component

---
+++ src/components/molecules/Header/constants.ts
@@ -0,0 +1,52 @@
+export const BUTTONS = [
+  {
+    name: 'Explore',
+    active: true,
+    id: 1
+  },
+  {
+    name: 'Plugins',
+    active: false,
+    id: 2
+  },
+  {
+    name: 'Design systems',
+    active: false,
+    id: 3
+  },
+  {
+    name: 'Wireframes',
+    active: false,
+    id: 4
+  },
+  {
+    name: 'Illustrations',
+    active: false,
+    id: 5
+  },
+  {
+    name: 'Icons',
+    active: false,
+    id: 6
+  },
+  {
+    name: 'Typography',
+    active: false,
+    id: 7
+  },
+  {
+    name: 'Movile design',
+    active: false,
+    id: 8
+  },
+  {
+    name: 'Web design',
+    active: false,
+    id: 9
+  },
+  {
+    name: 'UI kits',
+    active: false,
+    id: 10
+  },
+]
---
+++ src/components/molecules/Header/index.tsx
@@ -0,0 +1,45 @@
+import React from "react"
+import './styles.css'
+
+import Button from "../../atoms/Button"
+import Input from "../../atoms/Input"
+
+import { SearchIcon } from '../../../assets/SearchIcon'
+
+import { BUTTONS } from './constants'
+
+interface Props {
+  title: string;
+  subtitle: string;
+  activeSection: string;
+}
+
+const Header: React.FC<Props> = ({ title, subtitle, activeSection }) => {
+  return (
+    <div className="header--container">
+      <div className="header--buttons">
+        {
+          BUTTONS.map(({ name, id }) => (
+            <Button key={id} content={name} active={activeSection === name ? true : false}/>
+          ))
+        }
+      </div>
+      <h1 className="header--title">{title}</h1> 
+      <h4 className="header--subtitle">{subtitle}</h4>
+      <div className="header--form">
+        <div className='header--form--cont'>
+          <form>
+            <div className="header--input">
+              <Input placeholder="Search for files, plugins, and creators"/>
+              <div className="search--icon">
+                <SearchIcon/>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
