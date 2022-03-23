6321b3c57d646da1a0a193cb048f9cfe42c4d83e
Jonatan Luis Moreno <jonatanluismoreno@gmail.com>
2022-03-23 00:40:45 -0300

    Minimal Changes & assets

---
+++ src/constants.ts
@@ -0,0 +1,85 @@
+export const PLUGINS = [
+  {
+    name: 'Offset Resizer',
+    description: 'Offset frames, components and component sets by a uniform distance from their contents.',
+    image: 'plugin2',
+    id: null,
+  },
+  {
+    name: 'Auto Layout Grids',
+    description: '💪🏻 Use the native Figma »Layout Grids« together with Auto Layout!',
+    image: 'plugin3',
+    id: null,
+  },
+  {
+    name: 'Automator',
+    description: 'Automate your Figma workflows.Automator is currently in private beta. Join the waitlist to be one of the first to use Automator.See the Automator Playground file to learn more about how Automator works.Learn more at automator.design. Made by Tricycle.',
+    image: 'plugin4',
+    id: null,
+  },
+  {
+    name: 'Ghost',
+    description: 'Ghost. Ghost is a lightweight plugin that converts high fidelity mockups to loading or “skeleton” screens. How it Works. Ghost detects all text and shape layers (i.e., rectangles, ellipses, vectors, frames, etc.) selected. It then measures their dimensions before replacing them with rectangles; the original layers are removed. To generate a layout faithful to the original, Ghost removes auto-layout, thereby preventing some of the anomalies that occur when inserting children into frames. Basic Usage. Select a shape, text box, or an entire screen.Select a color. Ghost!',
+    image: 'plugin5',
+    id: null,
+  },
+  {
+    name: '',
+    description: '',
+    image: '',
+    id: null,
+  },
+  {
+    name: '',
+    description: '',
+    image: '',
+    id: null,
+  },
+  {
+    name: '',
+    description: '',
+    image: '',
+    id: null,
+  },
+  {
+    name: '',
+    description: '',
+    image: '',
+    id: null,
+  },
+  {
+    name: '',
+    description: '',
+    image: '',
+    id: null,
+  },
+  {
+    name: '',
+    description: '',
+    image: '',
+    id: null,
+  },
+  {
+    name: '',
+    description: '',
+    image: '',
+    id: null,
+  },
+  {
+    name: '',
+    description: '',
+    image: '',
+    id: null,
+  },
+]
+
+export const CARDS = [
+  {
+    title: 'Figma Auto Layout playground',
+    author: 'Figma',
+    likes: 3800,
+    duplicates: 108000,
+    image: 'main2',
+    icon: 'profilefigma'
+  },
+]
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,20 @@
+declare module '*.scss';
+
+declare module '*.png' {
+  const value: any
+  export = value;
+}
+
+declare module '*.jpg' {
+  const value: any
+  export = value;
+}
+
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>
+  const src: string
+  export default src
+}
+
 /// <reference types="vite/client" />
