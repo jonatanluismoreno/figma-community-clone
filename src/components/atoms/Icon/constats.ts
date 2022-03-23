acc2a0e7f290c3b3f78195d007bbc8a2dbab9fb6
Jonatan Luis Moreno <jonatanluismoreno@gmail.com>
2022-03-23 00:29:35 -0300

    Icon Component

---
+++ src/components/atoms/Icon/constats.ts
@@ -0,0 +1,13 @@
+import profile1 from '../../../assets/icon-profile1.png'
+import profile2 from '../../../assets/icon-profile2.jpg'
+import profile3 from '../../../assets/icon-profile1.jpg'
+import profile4 from '../../../assets/icon-profile1.jpg'
+
+
+export const IMAGES = [
+  profile1,
+  profile2,
+  profile3,
+  profile4
+]
+
