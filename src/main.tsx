e68f2818d8fc9fc716b3a5c7526b9ae74edfd5ff
Jonatan Luis Moreno <jonatanluismoreno@gmail.com>
2022-03-28 03:31:25 -0300

    Widgets Page Route

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import PluginsPage from "./pages/Plugins";
 import ExplorePage from "./pages/Explore";
+import WidgetsPage from "./pages/Widgets";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
@@ -13,6 +14,13 @@ ReactDOM.render(
         <Route path="/" element={<ExplorePage />} />
         <Route path="/explore" element={<ExplorePage />} />
         <Route path="/plugins" element={<PluginsPage />} />
+        <Route path="/widgets" element={<WidgetsPage />} />
+        <Route path="/figjam" element={<ExplorePage />} />
+        <Route path="/designsystems" element={<ExplorePage />} />
+        <Route path="/wireframe" element={<ExplorePage />} />
+        <Route path="/illustrations" element={<ExplorePage />} />
+        <Route path="/icons" element={<ExplorePage />} />
+        <Route path="/typography" element={<ExplorePage />} />
       </Routes>
     </React.StrictMode>
   </BrowserRouter>,
